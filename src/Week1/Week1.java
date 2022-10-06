97db3c9ba533412d4b9577adec44d44ce2c7aa52
triciaglassborow <22212293@bucks.ac.uk>
2022-10-06 22:01:55 +0100

    week1 up to task 9

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -4,6 +4,8 @@ import helpers.InputReader;
 public class Week1 
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
+    public static final int DAYS_YEAR = 365;
+    public static final int DAYS_MONTH = 30;
     public static void main(String[] args)
     {
         System.out.println(CONSOLE_CLEAR);
@@ -17,7 +19,40 @@ public class Week1
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
 
-        String name = InputReader.getString("Enter your name > ");
-        System.out.println("Hello " + name);
-    } 
+          //task 1
+          //System.out.println("hello world");
+          //task 2
+          //String name = InputReader.getString("Enter your name > ");
+          //System.out.println("Hello " + name);
+          //task 3
+          //double height = InputReader.getDouble("enter rectangle height: ");
+          //double length = InputReader.getDouble("enter rectangle length: ");
+          //System.out.println("area of the rectangle is " + (height*length));
+          //System.out.println("perimeter of the rectangle is "+(height+height+length+length));
+          //task 4
+          //double no1 = InputReader.getDouble("pick your first number: ");
+          //double no2 = InputReader.getDouble("pick your second number: ");
+          //System.out.println("the average of the two numbers picked is "+ ((no1+no2)/2));
+          //task 5
+          //int firstno = InputReader.getInt("pick a number: ");
+          //int secondno = InputReader.getInt("pick a different number: ");
+          //System.out.println(firstno+" "+secondno);
+          //System.out.println(secondno+" "+firstno);
+          //task 6 
+          //int age = InputReader.getInt("how old are you: ");
+          //System.out.println(2022-age);
+          //task 7
+          //System.out.println("put your date of birth");
+          //int day = InputReader.getInt("day: ");
+          //int month = InputReader.getInt("month: ");
+          //int year = InputReader.getInt("year: ");
+          //System.out.println((day+(month*DAYS_MONTH)+((2022-year)*DAYS_YEAR)));
+          //task 8
+          //double feet = InputReader.getDouble("how many feet?: ");
+          //System.out.println(feet/5280 + " miles");
+          
+
+  
+    }
 }
+       
